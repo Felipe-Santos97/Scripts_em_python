e075b3abb937782c5852d1ab7eeef0c632ee2c15
Felipe Santos <felipe.pier7@gmail.com>
2022-06-08 08:41:29 -0300

    adicionado numero 0800

--- coleta_contatos.py
+++ coleta_contatos.py
@@ -21,11 +21,13 @@ regex_numero_brasil = re.compile(r'''(
   (\d{4,}) 
 )''', re.VERBOSE)
 
-
-telefones = []
-for i in regex_numero_brasil.findall(texto_copiado):
-  phone = '-'.join([i[0]])
-  telefones.append(phone)
+regex_numero_0800 = re.compile(r'''(
+  (\d{4})
+  (\s|.|-)?
+  (\d{3,})
+  (\s|-|.)?
+  (\d{3,})
+)''' , re.VERBOSE)
 
 
 # Pega os emails e adiciona numa lista
@@ -33,12 +35,21 @@ emails = []
 for indice in email_regex.findall(texto_copiado):
   emails.append(indice[0])
 
+# Pega os telefones e adiciona numa lista
+telefones = []
+for indice in regex_numero_brasil.findall(texto_copiado):
+  telefones.append(indice[0])
+
+tel_0800 = []
+for indice in regex_numero_0800.findall(texto_copiado):
+  tel_0800.append(indice[0])
+
 # Remove duplicadas
 emails_uniq = list(set(emails))
 telefones_uniq = list(set(telefones))
 
 # Exibe os dados 
-if len(emails_uniq) == 0 and len(telefones_uniq) == 0:
+if len(emails_uniq) == 0 and len(telefones_uniq) == 0 and len(tel_0800) == 0:
   print('\nNão achado nenhum contado...\n')
   quit()
 
@@ -50,4 +61,7 @@ if len(telefones_uniq) > 0:
   print('\n========== TELEFONES ==========\n')
   print('\n'.join(telefones_uniq))
  
+if len(tel_0800) > 0:
+  print('\n'.join(tel_0800))
+
 print('\n')
